941d9fc5a3a292fb166c24ed8f4194fb4e3cde98
Ysabel Garcia <ysabel.garcia@live.com>
2021-09-10 01:55:37 +0000

    first commit for responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,65 +27,14 @@ $(() => {
 
   loadTweets();
 
-  //create a single post/node
-  const createTweetElement = (tweet) => {
-    const escape = function (str) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-    
-    //jquery take this stringified HTML and is read as HTML by the browser?
-    const $tweetHTML = 
-    `<header class="icon-name-handle">
-      <div class="icon-with-name">
-        <img class="avatar" src="${tweet.user.avatars}">
-        <h2 class="username">${tweet.user.name}</h2>
-      </div>
-      <h2 class="handle">${tweet.user.handle}</h2>
-    </header>
-    <p class="post">
-      ${escape(tweet.content.text)}
-    </p>
-    <footer class="time-with-icons">
-      <h5 class="time-since-posted">${timeago.format(tweet.created_at)}</h5>
-      <section class="interact-icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </section>
-    </footer>`;
-  
-    
-    const $tweetContainer = $('<article>').addClass('tweet-container');
-
-    let $tweet = $tweetContainer.append($tweetHTML);
-    console.log("After createTweetElement")
-    return $tweet;
-  }
-
-  const renderTweets = function(tweets) {
-    $mainContainer = $('.all-posts-container');
-    $mainContainer.empty();
-
-    console.log("during renderTweets")
-
-    for (const aTweet of tweets) {
-      console.log("aTweet",aTweet)
-      const $aTweet = createTweetElement(aTweet);
-
-      $mainContainer.prepend($aTweet);
-    }
-  }
-
   const $form = $('.new-tweet-form');
   $form.on('submit', function(event) {
     event.preventDefault();
     console.log('the form has been submitted');
-
+  
     const serializedData = $(this).serialize();
     console.log(serializedData);
-
+  
     //if input is empty -->
     if (!$('.tweet-text-area').val()) {
       //add a new class before rendering error message
@@ -114,3 +63,55 @@ $(() => {
   })
 });
 
+
+//create a single post/node
+const createTweetElement = (tweet) => {
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  
+  //jquery take this stringified HTML and is read as HTML by the browser?
+  const $tweetHTML = 
+  `<header class="icon-name-handle">
+    <div class="icon-with-name">
+      <img class="avatar" src="${tweet.user.avatars}">
+      <h2 class="username">${tweet.user.name}</h2>
+    </div>
+    <h2 class="handle">${tweet.user.handle}</h2>
+  </header>
+  <p class="post">
+    ${escape(tweet.content.text)}
+  </p>
+  <footer class="time-with-icons">
+    <h5 class="time-since-posted">${timeago.format(tweet.created_at)}</h5>
+    <section class="interact-icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </section>
+  </footer>`;
+
+  
+  const $tweetContainer = $('<article>').addClass('tweet-container');
+
+  let $tweet = $tweetContainer.append($tweetHTML);
+  console.log("After createTweetElement")
+  return $tweet;
+  
+}
+
+const renderTweets = function(tweets) {
+  $mainContainer = $('.all-posts-container');
+  $mainContainer.empty();
+
+  console.log("during renderTweets")
+
+  for (const aTweet of tweets) {
+    console.log("aTweet",aTweet)
+    const $aTweet = createTweetElement(aTweet);
+
+    $mainContainer.prepend($aTweet);
+  }
+}
